5188a23f393c3aa85f7a728a520d86f3cae83161
Abhiman Raj <abhimaanraj90@gmail.com>
2026-04-15 16:08:52 +0530

    updated navbar svg icons

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -35,26 +35,46 @@ export function Navbar() {
 
           <nav className="navbar-center">
             <ul>
-              <li><a href="/categories">Best Products</a></li>
-              <li><a href="/leaderboard">Launches</a></li>
-              <li><a href="/newsletters">News</a></li>
-              <li><a href="/forums">Forums</a></li>
+              <li>
+                <a href="/categories">
+                  Best Products
+                  <svg width="14" height="14" viewBox="0 0 14 14" fill="none" stroke="currentColor" strokeWidth="2">
+                    <path d="M3.5 6.25 7 9.75l3.5-3.5" />
+                  </svg>
+                </a>
+              </li>
+              <li><a href="/leaderboard">Launches
+                               <svg width="14" height="14" viewBox="0 0 14 14" fill="none" stroke="currentColor" strokeWidth="2">
+                    <path d="M3.5 6.25 7 9.75l3.5-3.5" />
+                  </svg>
+              </a></li>
+              <li><a href="/newsletters">News
+                               <svg width="14" height="14" viewBox="0 0 14 14" fill="none" stroke="currentColor" strokeWidth="2">
+                    <path d="M3.5 6.25 7 9.75l3.5-3.5" />
+                  </svg>
+              </a></li>
+              <li><a href="/forums">Forums
+                               <svg width="14" height="14" viewBox="0 0 14 14" fill="none" stroke="currentColor" strokeWidth="2">
+                    <path d="M3.5 6.25 7 9.75l3.5-3.5" />
+                  </svg>
+              </a></li>
               <li className="hide-mobile">
-                <a href="/sponsor">Advertise</a>
+                <a href="/sponsor">Advertise
+                               <svg width="14" height="14" viewBox="0 0 14 14" fill="none" stroke="currentColor" strokeWidth="2">
+                    <path d="M3.5 6.25 7 9.75l3.5-3.5" />
+                  </svg>
+                </a>
               </li>
             </ul>
           </nav>
           
           <div className="navbar-right">
             <a href="/newsletters" className="btn-outline">
-              <svg width="16" height="16" className="ph-btn-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-                <polyline points="22,6 12,13 2,6"></polyline>
-              </svg>
+              <svg width="20" height="20" fill="none" viewBox="0 0 20 20" class="size-4"><path stroke="#344054" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.667" d="m11.453 2.195 6.274 4.077c.221.144.332.216.412.313a.8.8 0 0 1 .158.289c.036.12.036.252.036.516v6.11c0 1.4 0 2.1-.272 2.635a2.5 2.5 0 0 1-1.093 1.092c-.535.273-1.235.273-2.635.273H5.667c-1.4 0-2.1 0-2.635-.273a2.5 2.5 0 0 1-1.093-1.092c-.272-.535-.272-1.235-.272-2.635V7.39c0-.264 0-.396.036-.516a.8.8 0 0 1 .157-.29c.08-.096.192-.168.413-.312l6.274-4.077m2.906 0c-.526-.342-.789-.513-1.072-.58a1.7 1.7 0 0 0-.762 0c-.283.067-.546.238-1.072.58m2.906 0 5.16 3.354c.574.372.86.559.96.795a.83.83 0 0 1 0 .645c-.1.237-.386.423-.96.796l-5.16 3.354c-.526.342-.789.513-1.072.58a1.7 1.7 0 0 1-.762 0c-.283-.067-.546-.238-1.072-.58l-5.16-3.354c-.574-.373-.86-.56-.96-.796a.83.83 0 0 1 0-.645c.1-.236.386-.423.96-.795l5.16-3.354m9.37 13.638-5.536-5m-4.762 0-5.536 5"></path></svg>
               Subscribe
             </a>
             <button className="btn-primary">
-              <svg width="16" height="16" className="ph-btn-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+              <svg width="14" height="14" className="ph-btn-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                 <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
                 <polyline points="10 17 15 12 10 7"></polyline>
                 <line x1="15" y1="12" x2="3" y2="12"></line>
