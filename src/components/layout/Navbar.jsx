05b92c1146d4ef1eef7994165f5c07ae37dffd64
esachdev28 <esachdev5@gmail.com>
2026-04-23 19:03:17 +0530

    fixed deployment issue for advertise page

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { SearchModal } from "../Modal/Modal";
 
@@ -162,14 +163,14 @@ export function Navbar({ searchTerm, setSearchTerm, onSubscribe }) {
         <div className="navbar-container">
 
           <div className="navbar-left">
-            <a href="/" className="logo">
+            <Link to="/" className="logo">
               <svg xmlns="http://www.w3.org/2000/svg" width="52" height="52" viewBox="0 0 45 45" className="size-10 dark:hidden">
                 <g fill="none" fillRule="evenodd">
                   <path fill="#FF6154" d="M40 20c0 11.046-8.954 20-20 20S0 31.046 0 20 8.954 0 20 0s20 8.954 20 20"></path>
                   <path fill="#FFF" d="M22.667 20H17v-6h5.667a3 3 0 0 1 0 6m0-10H13v20h4v-6h5.667a7 7 0 1 0 0-14"></path>
                 </g>
               </svg>
-            </a>
+            </Link>
 
             <div className="search-box" onClick={() => setIsModalOpen(true)}>
               <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -200,7 +201,7 @@ export function Navbar({ searchTerm, setSearchTerm, onSubscribe }) {
               <NavItemWithDropdown title="News" items={NAV_DROPDOWNS.news} href="/newsletters" />
               <NavItemWithDropdown title="Forums" items={NAV_DROPDOWNS.forums} href="/forums" />
               <li className="hide-mobile">
-                <a href="/advertise">Advertise</a>
+                <Link to="/advertise">Advertise</Link>
               </li>
             </ul>
           </nav>
